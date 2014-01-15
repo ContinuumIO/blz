c9349ca3357188bd1fa04d1c687ea7d39c62e955
Mark Wiebe <mwiebe@continuum.io>
2014-01-15 11:05:07 -0800

    Build fix for MSVC 2008

--- c-blosc/internal-complibs/snappy-1.1.1/snappy-stubs-public.h
+++ c-blosc/internal-complibs/snappy-1.1.1/snappy-stubs-public.h
@@ -36,8 +36,19 @@
 #ifndef UTIL_SNAPPY_OPENSOURCE_SNAPPY_STUBS_PUBLIC_H_
 #define UTIL_SNAPPY_OPENSOURCE_SNAPPY_STUBS_PUBLIC_H_
 
-#if 1
+#if !defined(_MSC_VER) || _MSC_VER >= 1600
 #include <stdint.h>
+#else
+typedef signed char      int8_t;
+typedef short            int16_t;
+typedef int              int32_t;
+typedef __int64          int64_t;
+typedef ptrdiff_t        intptr_t;
+typedef unsigned char    uint8_t;
+typedef unsigned short   uint16_t;
+typedef unsigned int     uint32_t;
+typedef unsigned __int64 uint64_t;
+typedef size_t           uintptr_t;
 #endif
 
 #if 1
